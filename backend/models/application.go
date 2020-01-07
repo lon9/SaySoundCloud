11182323bc71c331917ce22b4ff2386be31c9eab
lon9 <rompei0924@gmail.com>
2020-01-07 21:24:45 +0900

    Implement models.

--- backend/models/application.go
+++ backend/models/application.go
@@ -1,7 +1,10 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
+	"github.com/lon9/soundboard/backend/database"
 )
 
 // Application is struct of application
@@ -14,4 +17,44 @@ type Application struct {
 	UserID      uint   `json:"userId"`
 	Description string `json:"description"`
 	AccessToken string `json:"accessToken"`
+}
+
+// Create creates an application
+func (a *Application) Create() (err error) {
+	db := database.GetDB()
+	return db.Create(a).Error
+}
+
+// FindByID finds an application by id
+func (a *Application) FindByID(id uint) (err error) {
+	db := database.GetDB()
+	return db.Where("id = ?", id).First(a).Error
+}
+
+// Update updates an application
+func (a *Application) Update() (err error) {
+	db := database.GetDB()
+	return db.Save(a).Error
+}
+
+// Delete deletes an application
+func (a *Application) Delete() (err error) {
+	db := database.GetDB()
+	return db.Delete(a).Error
+}
+
+// Applications is slice of applications
+type Applications []Application
+
+// List lists applications
+func (as *Applications) List(offset, limit int) (err error) {
+	db := database.GetDB()
+	return db.Order("name asc").Limit(limit).Offset(offset).Find(as).Error
+}
+
+// SearchByName searches applications by name
+func (as *Applications) SearchByName(name string, limit, offset int) (err error) {
+	db := database.GetDB()
+	query := fmt.Sprintf("%%%s%%", name)
+	return db.Where("name LIKE ?", query).Order("name asc").Limit(limit).Offset(offset).Find(as).Error
 }
--- backend/models/user.go
+++ backend/models/user.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/lon9/soundboard/backend/database"
 )
 
 // User is struct of user
@@ -13,6 +14,29 @@ type User struct {
 	Description  string        `json:"description"`
 }
 
+// Create creates a user
 func (u *User) Create() (err error) {
-	return
+	db := database.GetDB()
+	return db.Create(u).Error
 }
+
+// FindByUID finds a user by uid
+func (u *User) FindByUID(uid string) (err error) {
+	db := database.GetDB()
+	return db.Where("uid = ?", uid).First(u).Error
+}
+
+// Update updates user
+func (u *User) Update() (err error) {
+	db := database.GetDB()
+	return db.Save(u).Error
+}
+
+// Delete deletes a user
+func (u *User) Delete() (err error) {
+	db := database.GetDB()
+	return db.Delete(u).Error
+}
+
+// Users is slice of users
+type Users []User
